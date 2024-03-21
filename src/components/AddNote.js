5fd96f7e1ad795e98c4a4b4945c4be63c951931f
siddhanv7 <siddhanv567@gmail.com>
2024-03-21 10:37:20 +0530

    adding edit functionality

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,13 +1,34 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 // import react from 'react'
 import noteContext from '../context/notes/noteContext';
 
 
-const AddNote = () => {
+const AddNote = (props) => {
     const context = useContext(noteContext);
-    const { addNote } = context;
-    const [note, setNote] = useState({ title: "dd", description: "dd", tag: "TAG" })
+    const { addNote, edit, setEdit, editNote } = context;
+    // const { title, description, tags  } = props;
+    const [note, setNote] = useState({ title: "", description: "", tag: "" })
 
+    useEffect(() => {
+        if (edit.change === "True") {
+            setNote({
+                title: edit.title,
+                description: edit.description,
+                tag: edit.tag
+            });
+        } else {
+            setNote({ title: "", description: "", tag: "" });
+        }
+    }, [edit]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (edit.change === "True") {
+            handleEdit();
+        } else {
+            handleClick();
+        }
+    };
     const handleClick = (e) => {
         e.preventDefault();
         addNote({
@@ -16,26 +37,39 @@ const AddNote = () => {
             tag: note.tag
         });
     };
+
+    const handleEdit = () => {
+        editNote({
+            id: edit.id,
+            title: note.title,
+            description: note.description,
+            tag: note.tag
+        });
+        setEdit({ ...edit, change: "False" });
+        setNote({ title: "", description: "", tag: "" });
+    };
     const onChange = (e) => {
         setNote({ ...note, [e.target.name]: e.target.value })
     }
-    const handleChange = (e) => {
-        setNote({ ...note, [e.target.name]: e.target.value });
-    }
+
     return (
         <div>
             <div className="container">
-                <h4>Add a Note</h4>
-                <form className='mx-3'>
+            <h4>{edit.change === "True" ? 'Edit Note' : 'Add a Note'}</h4>
+                <form className='mx-3' onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="title" name="title" value={note.title} onChange={handleChange}/>
+                        <input type="text" className="form-control" id="title" name="title" value={note.title} onChange={onChange} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Description</label>
                         <input type="text" className="form-control" id="description" name="description" value={note.description} onChange={onChange} />
                     </div>
-                    <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
+                    <div className="mb-3">
+                        <label htmlFor="tag" className="form-label">Tag</label>
+                        <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={handleClick}>{edit.change === "True" ? 'Update' : 'Submit'}</button>
                 </form>
                 <h4>Your Notes</h4>
             </div>
